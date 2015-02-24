115a13acdbb0cec7ba95a47e3de7cb4e5d791cc2
Russell White <rarctem@gmail.com>
2015-02-24 11:14:23 -0700

    Update speed to solve Challenge #6.

--- speed.js
+++ speed.js
@@ -11,78 +11,42 @@
 
         for(var i = 0; i < elevators.length; i++) {
             var elevator = elevators[i];
-            elevator.delivering = false;
+            //elevator.delivering = false;
 
             elevator.on("idle", function() {
-                if(this.loadFactor() > 0.75)
-                    this.delivering = true;
-                else if(this.loadFactor() == 0) {
-                    this.delivering = false;
-                    this.goingUpIndicator(true);
-                    this.goingDownIndicator(true);
-                }
-
-                if(anyWaiting(floors) && !this.delivering) {
-                    var closest = null;
-                    for(var j = 0; j < floors.length; j++) {
-                        var floor = floors[j];
-                        if((floor.up || floor.down) && floor.claimed == false) {
-                            if(closest == null || Math.abs(closest - this.currentFloor()) > Math.abs(floor.floorNum() - this.currentFloor())) {
-                                if(closest != null)
-                                    floors[closest].claimed = false;
-                                floor.claimed = this;
-                                closest = floor.floorNum();
-                            }
+                if(this.currentFloor() == 0) {
+                    if(this.getPressedFloors().length > 0) {
+                        for(var j = 0; j < this.getPressedFloors().length; j++) {
+                            this.goToFloor(this.getPressedFloors()[j]);
                         }
                     }
-
-                    this.goToFloor(closest);
                 } else {
-                    this.delivering = true;
-                    var closest = null;
-                    for(var j = 0; j < this.getPressedFloors().length; j++) {
-                        var floor = this.getPressedFloors()[j];
-                        if(closest == null || Math.abs(closest - this.currentFloor()) > Math.abs(floor - this.currentFloor())) {
-                            closest = floor;
-                        }
-                    }
-
-                    if(closest > this.currentFloor()) {
-                        this.goingUpIndicator(true);
-                        this.goingDownIndicator(false);
-                    } else {
-                        this.goingUpIndicator(false);
-                        this.goingDownIndicator(true);
-                    }
-                    this.goToFloor(closest);
+                    this.goToFloor(0);
                 }
-
-                // if(this.destinationQueue == []) {
-                //     this.goToFloor(floor(i / elevators.length * floors.length));
-                // }
             });
             elevator.on("stopped_at_floor", function(floorNum) {
+                floors[floorNum].up = false;
+                floors[floorNum].down = false;
+
                 if(floors[floorNum].claimed == this) {
-                    floors[floorNum].up = false;
-                    floors[floorNum].down = false;
                     floors[floorNum].claimed = false;
                 }
             });
             elevator.on("passing_floor", function(floorNum, direction) {
-                if(((direction == "up" && floors[floorNum].up) ||
-                    (direction == "down" && floors[floorNum].down)) &&
-                    this.loadFactor() < 0.5 && floors[floorNum].claimed == null) {
-                    this.goToFloor(floorNum, true);
-                }
+                // if(((direction == "up" && floors[floorNum].up) ||
+                //     (direction == "down" && floors[floorNum].down)) &&
+                //     this.loadFactor() < 0.5 && floors[floorNum].claimed == null) {
+                //     this.goToFloor(floorNum, true);
+                // }
             });
         }
-        
+
         for(var i = 0; i < floors.length; i++) {
             var floor = floors[i];
             floor.up = false;
             floor.down = false;
             floor.claimed = false;
-            
+
             floor.on("up_button_pressed", function() {
                 this.up = true;
             });
