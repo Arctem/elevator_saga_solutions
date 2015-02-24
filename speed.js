57036da1953ecd06d2236e9afd480998c15a4cf8
Russell White <rarctem@gmail.com>
2015-02-24 12:03:22 -0700

    Update speed to solve #12.

--- speed.js
+++ speed.js
@@ -15,16 +15,20 @@
 
             elevator.on("idle", function() {
                 if(this.currentFloor() == 0 && this.getPressedFloors().length > 0) {
-                    for(var j = 0; j < this.getPressedFloors().length; j++) {
-                        this.goToFloor(this.getPressedFloors()[j]);
-                        if(floors[j].claimed == false) {
-                            floors[j].claimed = this;
-                        }
+                    var pressed = this.getPressedFloors();
+                    pressed.sort();
+                    for(var j = 0; j < pressed.length; j++) {
+                        this.goToFloor(pressed[j]);
+                    }
+                    if(floors[0].claimed == false) {
+                        floors[0].claimed = this;
+                        console.log("Claimed " + j);
                     }
                 } else if(anyWaiting(floors.slice(1))) {
                     for(var j = floors.length - 1; j > 0; j--) {
                         if((floors[j].down || floors[j].up) && floors[j].claimed == false) {
                             floors[j].claimed = this;
+                            console.log("Claimed " + j);
                             this.goToFloor(j);
                             break;
                         }
@@ -44,17 +48,32 @@
                 }
 
                 //Check for any unqueued pressed floors
-                for(var j = 0; j < this.getPressedFloors().length; j++) {
-                    var num = this.getPressedFloors()[j];
+                var pressed = this.getPressedFloors();
+                pressed.sort();
+                for(var j = 0; j < pressed.length; j++) {
+                    var num = pressed[j];
                     if(this.destinationQueue.indexOf(num) == -1) {
                         this.goToFloor(num);
                     }
                 }
+
+                if(this.destinationQueue.length != 0) {
+                    if(floors[this.destinationQueue[0]].claimed == false) {
+                        floors[this.destinationQueue[0]].claimed = this;
+                        console.log("Claimed " + j);
+                    }
+                }
             });
             elevator.on("passing_floor", function(floorNum, direction) {
-                if(((direction == "up" && floors[floorNum].up) ||
+                if((((direction == "up" && floors[floorNum].up) ||
                     (direction == "down" && floors[floorNum].down)) &&
-                    this.loadFactor() < 0.5 && floors[floorNum].claimed == false) {
+                    this.loadFactor() < 0.75 && floors[floorNum].claimed == false) ||
+                    this.destinationQueue.indexOf(floorNum) != -1) {
+
+                    while(this.destinationQueue.indexOf(floorNum) != -1) {
+                        this.destinationQueue.splice(this.destinationQueue.indexOf(floorNum), 1)
+                    }
+                    this.checkDestinationQueue();
                     this.goToFloor(floorNum, true);
                 }
             });
